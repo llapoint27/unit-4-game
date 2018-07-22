d7e7aac292d3e10fb6935937b1cc8a1d597b5c8b
Marco Polo <laurenlapoint@Laurens-MacBook-Pro.local>
2018-07-22 12:19:32 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,46 @@
 //Crystal Javascript with jQuery
 
-//Assigning random values to each crystal
-var crystal1 = Math.floor(Math.random() * 12 + 1);
-var crystal2 = Math.floor(Math.random() * 12 + 1);
-var crystal3 = Math.floor(Math.random() * 12 + 1);
-var crystal4 = Math.floor(Math.random() * 12 + 1);
-// // console.log(crystal4);
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
+var random;
 
-//variables for tallying up score, NEED TO ADD ID'S
+function startGame() {
+//Assigning random values to each crystal
+crystal1 = genRandNumTwelve();
+crystal2 = genRandNumTwelve();
+crystal3 = genRandNumTwelve();
+crystal4 = genRandNumTwelve();
+random = genRandNumTarget();
+$("#randomNum").text("Number: " + random);
+
+}
+
+function genRandNumTwelve() {
+    return Math.floor(Math.random() * 12 + 1);
+};
+function genRandNumTarget() {
+    return Math.floor(Math.random() * 101 + 19);
+};
+console.log(genRandNumTarget())
+
+//variables for tallying up total/win/losses
 var userTotal = 0;
 var wins = 0;
 var losses = 0;
 
 $(document).ready(function () {
 
-
-    var random = Math.floor(Math.random() * 102 + 19);
-    $("#randomNum").text("Number: " + random);
+    startGame();
 
 
-    //When you click on crystals the assigned random value 'crystal1,2,3,4' will add to 'your score'. The values are hidden because assigned to .text on 'your score' instead of crystal images. The values stay the same number in the duration of each round. 
+    //When you click on crystals the assigned random value 'crystal1,2,3,4' will add to 'your score'. The values are hidden because assigned to .text on 'your score' instead of crystal images. The values stay for the duration of each round. 
 
+    // $(.class).on(click function (){
+    //     $(this).attr('data-value')
+    // });
+   
     $("#amethyst").click(function () {
         userTotal = userTotal + crystal1;
         tallies();
@@ -45,35 +65,21 @@ $(document).ready(function () {
         $("#yourScore").text("Your Score: " + userTotal);
     });
 
-
+    //If user is able to match the numbers, then this function is executed. Win's is increased by 1, userTotal is reset, and the funtions: resetRandom and resetCrystal will execute. 
     function tallies() {
 
         if (userTotal === random) {
             $("#wins").text("Wins: " + (wins += 1));
             userTotal = 0;
-            resetRandom();
-            resetCrystal();
+            startGame();
 
+    //Is user guesses over the random number, the losses increases by 1, userTotal is reset to 0, and the functions below are executed. 
         } else if (userTotal > random) {
             $("#losses").text("Losses: " + (losses += 1));
             userTotal = 0;
-            resetRandom();
-            resetCrystal();
+            startGame();
         }
 
-    }
-
-    function resetRandom() {
-
-        random = Math.floor(Math.random() * 102 + 19);
-        $("#randomNum").text("Number: " + random);
-    }
-
-    function resetCrystal() {
-        crystal1 = Math.floor(Math.random() * 12 + 1);
-        crystal2 = Math.floor(Math.random() * 12 + 1);
-        crystal3 = Math.floor(Math.random() * 12 + 1);
-        crystal4 = Math.floor(Math.random() * 12 + 1);
     }
 
 });
