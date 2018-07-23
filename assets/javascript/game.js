a8a79b58e4ae66d19f67039ccfe46fb0e0e3c3f0
Marco Polo <laurenlapoint@Laurens-MacBook-Pro.local>
2018-07-22 22:25:15 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,37 @@
 //Crystal Javascript with jQuery
 
+//global varibles
 var crystal1;
 var crystal2;
 var crystal3;
 var crystal4;
 var random;
 
+//crystals are assigned their random values, along with a random number assigned for the user to guess 
+
 function startGame() {
-//Assigning random values to each crystal
+
 crystal1 = genRandNumTwelve();
 crystal2 = genRandNumTwelve();
 crystal3 = genRandNumTwelve();
 crystal4 = genRandNumTwelve();
 random = genRandNumTarget();
+
 $("#randomNum").text("Number: " + random);
 
 }
 
+//funtion to assign a random number between 1-12
 function genRandNumTwelve() {
     return Math.floor(Math.random() * 12 + 1);
 };
+//function to assign a random number between 19-120
 function genRandNumTarget() {
     return Math.floor(Math.random() * 101 + 19);
 };
 console.log(genRandNumTarget())
 
-//variables for tallying up total/win/losses
+//variables for tallying up userTotal/win/losses
 var userTotal = 0;
 var wins = 0;
 var losses = 0;
@@ -35,7 +41,7 @@ $(document).ready(function () {
     startGame();
 
 
-    //When you click on crystals the assigned random value 'crystal1,2,3,4' will add to 'your score'. The values are hidden because assigned to .text on 'your score' instead of crystal images. The values stay for the duration of each round. 
+    //The random value assigned to the crystals will add to userTotal. The values will remain hidden and do not change within a round. 
 
     // $(.class).on(click function (){
     //     $(this).attr('data-value')
@@ -65,7 +71,7 @@ $(document).ready(function () {
         $("#yourScore").text("Your Score: " + userTotal);
     });
 
-    //If user is able to match the numbers, then this function is executed. Win's is increased by 1, userTotal is reset, and the funtions: resetRandom and resetCrystal will execute. 
+    //If user is able to match the numbers, then Win's is increased by 1, userTotal is reset, and the game will start over with the function: startGame. 
     function tallies() {
 
         if (userTotal === random) {
@@ -73,7 +79,7 @@ $(document).ready(function () {
             userTotal = 0;
             startGame();
 
-    //Is user guesses over the random number, the losses increases by 1, userTotal is reset to 0, and the functions below are executed. 
+    //If user guesses over the random number, then losses increases by 1, userTotal is reset to 0, and the game will start over with the function: startGame. 
         } else if (userTotal > random) {
             $("#losses").text("Losses: " + (losses += 1));
             userTotal = 0;
